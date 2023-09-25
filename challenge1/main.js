e108d531161a316a1e440d59d346ca9eca656dbc
Aldiandya Irsyad <dragonopy@gmail.com>
2023-09-25 12:18:48 +0700

    restructuring

---
+++ challenge1/main.js
@@ -0,0 +1,41 @@
+const data = [
+  {
+    name: "Alice",
+    birth_year: 2020
+  },
+  {
+    name: "Bob",
+    birth_year: 2010
+  },
+  {
+    name: "Charlie",
+    birth_year: 1950
+  },
+  {
+    name: "Daniel",
+    birth_year: 2000
+  },
+]
+
+const totalHadiah = 50;
+  
+function getRouletWinner(data) {
+  function gameLogic(data) {
+    const randomPerson = data[Math.floor(Math.random() * data.length)]
+
+    return randomPerson
+  }
+
+}
+
+function checkAgeWinner(person) {
+  const Output = [
+    diatas_50: 
+  ]
+  const age = 2023 - person.birth_year;
+
+  if (age >= 50) {
+
+
+  }
+}
